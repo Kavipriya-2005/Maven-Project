1116cc3a889704f117325925cb22b3fac5fa4cea
Kavipriya-2005 <kavipriya.27csa@licet.ac.in>
2026-02-24 10:40:20 +0530

    Initial commit

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(@ModelAttribute User user, Model model) {
+        model.addAttribute("message", "Registration Successful for " + user.getName());
+        return "success";
+    }
+}
